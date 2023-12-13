975a1f4ba136d6766185e6f5961f42db32f41bd9
Ariful2634 <ariful2634@gmail.com>
2023-12-13 23:19:40 +0600

    setting up the navbar

---
+++ src/components/shared/Navbar.js
@@ -0,0 +1,81 @@
+"use client"
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import logo from '@/assets/group.png'
+import Image from 'next/image';
+import { IconButton, Stack } from '@mui/material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import Link from 'next/link';
+
+const navItems = [
+    {
+        route:'Home',
+        pathname:'/'
+    },
+    {
+        route:'Pages',
+        pathname:'/pages'
+    },
+    {
+        route:'Category',
+        pathname:'/category'
+    },
+    {
+        route:'News',
+        pathname:'/news'
+    },
+    {
+        route:'Post',
+        pathname:'/post'
+    },
+    {
+        route:'Contact',
+        pathname:'/contact'
+    },
+];
+
+
+function Navbar() {
+  
+
+  return (
+    <AppBar position="static" className='bg-black'>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+            <Image src={logo} width={100} height={100} alt="logo"/>
+
+          <Box className="w-full text-center">
+            {navItems.map((item) => (
+             <Link key={item} href={item.pathname}>
+                <Button className='text-white'>{item.route}</Button>
+             </Link>
+            ))}
+          </Box>
+          <Box>
+          <Stack direction="row" sx={{ "& svg":{
+            color:"white"
+          } }}>
+          <IconButton>
+                <FacebookIcon></FacebookIcon>
+            </IconButton>
+            <IconButton>
+                <FacebookIcon></FacebookIcon>
+            </IconButton>
+            <IconButton>
+                <FacebookIcon></FacebookIcon>
+            </IconButton>
+            <IconButton>
+                <FacebookIcon></FacebookIcon>
+            </IconButton>
+          </Stack>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Navbar;
